40097b690c8b4817c81a95577b8d160eb4f3cf95
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-15 11:58:24 +0300

    perform simple smoke test

---
+++ K6-Beginner-01/k6-smoke-test-script-02.js
@@ -0,0 +1,19 @@
+import http from 'k6/http'
+import { sleep } from 'k6'
+
+export const options = {
+    vus: 1,
+    duration: "30s"
+}
+
+export default function() {
+    http.get('https://quickpizza.grafana.com/')
+
+    sleep(1)
+
+    http.get("https://quickpizza.grafana.com/login")
+    sleep(2)
+
+    http.get("https://quickpizza.grafana.com/admin")
+    sleep(3)
+} 
