cf53d44a5756be62365d2cc3ed971e2858cdcf35
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-10 11:36:30 +0300

    Adding k6 npm module

---
+++ K6-Beginner-01/k6-load-test-script-03.js
@@ -0,0 +1,18 @@
+import http from 'k6/http'
+import { sleep } from 'k6'
+
+export const options = {
+    vus: 1,
+    duration: "30s"
+}
+
+export default function() {
+    http.get('https://quickpizza.grafana.com/')
+    sleep(1)
+
+    http.get("https://quickpizza.grafana.com/login")
+    sleep(2)
+
+    http.get("https://quickpizza.grafana.com/admin")
+    sleep(3)
+} 
