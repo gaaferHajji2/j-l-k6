f50df7bd26fdb7f19ad868e0264a65c710ff9cad
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-10 12:25:41 +0300

    Perform spike test

---
+++ K6-Beginner-01/k6-spike-test-script-04.js
@@ -0,0 +1,24 @@
+import http from 'k6/http'
+import { sleep } from 'k6'
+
+export const options = {
+    stages: [
+        // This simulate real scenario for spike test
+        {
+            duration: '2m',
+            target: 10000
+        }, 
+        {
+            duration: '1m',
+            target: 0
+        },
+    ]
+    // vus: 100,
+    // duration: '30s'
+}
+
+export default function() {
+    // in spike test usually we call only one address
+    http.get('https://quickpizza.grafana.com/')
+    sleep(1)
+} 
