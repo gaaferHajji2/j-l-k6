1fb9d9f843fe248b5001ffad0b7063b9d136742c
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-30 15:32:09 +0300

    adding check with status

--- K6-Beginner-01/Make-Requests/http-scenario.js
+++ K6-Beginner-01/Make-Requests/http-scenario.js
@@ -1,9 +1,14 @@
 import http from 'k6/http'
+import { check } from 'k6'
 
 export default function() {
     const t1 = http.get('https://example.com/')
     console.log(`The status code ${t1.status}, and status text ${t1.status_text}`)
 
+    check(t1, { "jloka-01": (t1) => t1.status == 200})
+
     const t2 = http.get('https://example.com/test.php')
     console.log(`The status code ${t2.status}, and status text ${t2.status_text}`)
+
+    check(t2, {"jloka-02": (t2) => t2.status != 200})
 }
