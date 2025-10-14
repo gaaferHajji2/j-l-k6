998328ac0a351deabaa99785b46635164dfa9838
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-14 14:04:13 +0300

    Remove unused imports

--- K6-Beginner-01/Make-Requests/complete-custom-metrics.js
+++ K6-Beginner-01/Make-Requests/complete-custom-metrics.js
@@ -1,6 +1,6 @@
 import http from 'k6/http'
-import { check, sleep } from 'k6'
-import { Counter, Trend } from 'k6/metrics'
+import { sleep } from 'k6'
+import { Trend } from 'k6/metrics'
 
 export const options = {
   vus: 20,
